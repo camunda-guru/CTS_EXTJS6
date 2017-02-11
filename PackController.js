0a7371e5999dfb8ae5c2724a46f9e10ac073f0b2
eswaribala <Parameswaribala@gmail.com>
2017-02-11 14:31:35 +0530

    Add files via upload

---
+++ PackController.js
@@ -0,0 +1,22 @@
+﻿Ext.define('D3Pack.view.d3.PackController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.pack',
+
+    requires: [
+        'Ext.util.Format'
+    ],
+
+    onTooltip: function (component, tooltip, node, element, event) {
+        var size = node.get('task'),
+            n = node.childNodes.length;
+        console.log(size);
+
+
+        if (size) {
+            tooltip.setHtml(node.data.task);
+        } else {
+            tooltip.setHtml(n + ' file' + (n === 1 ? '' : 's') + ' inside.');
+        }
+    }
+
+});
---
+++ Packmain.js
@@ -0,0 +1,31 @@
+﻿Ext.define('D3Pack.view.d3.Pack', {
+    extend: 'Ext.Panel',
+    requires: [
+        'D3Pack.view.d3.TreeViewModel',
+        'Ext.d3.hierarchy.Pack'
+    ],
+
+
+    controller: 'pack',
+
+    viewModel: {
+        type: 'tree'
+    },
+
+    session: true,
+
+    cls: 'card1',
+    layout: 'fit',
+    shadow: true,
+
+    items: [{
+        xtype: 'd3-pack',
+        padding: 20,
+        bind: {
+            store: '{store}'
+        },
+        tooltip: {
+            renderer: 'onTooltip'
+        }
+    }]
+});
